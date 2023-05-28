b68e7c32d59d6f8de2bba26e1ee3f7efc9b45ef2
Kirill <frolkv@gmail.com>
2023-05-28 10:24:07 +0300

    Initial Commit

---
+++ My Car App/MainScreenViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  My Car App
+//
+//  Created by Kirill Frolovskiy on 28.05.2023.
+//
+
+import UIKit
+
+class MainScreenViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    title = "My car"
+    navigationController?.navigationBar.prefersLargeTitles = true
+    view.backgroundColor = .white
+  }
+
+
+}
+
